f3347e4ecb712966ef0ec710606b66a4c360ccd1
RutulTrivedi <rutultrivedi7@gmail.com>
2024-08-13 22:30:22 +0530

    Way Too Long Words

---
+++ WayTooLongWords.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class WayTooLongWords {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine(); // Consume the newline character after the integer input
+
+        for (int i = 0; i < n; i++) {
+            String s = sc.nextLine();
+            int len = s.length();
+
+            if (len > 10) {
+                System.out.println(s.charAt(0) + String.valueOf(len - 2) + s.charAt(len - 1));
+            } else {
+                System.out.println(s);
+            }
+        }
+    }
+}
