069937b9521362805a0a5e314ddfccaeb29b5cdf
RutulTrivedi <rutultrivedi7@gmail.com>
2024-08-18 11:15:40 +0530

    Chat Room

--- ChatRoom.java
+++ ChatRoom.java
@@ -2,24 +2,22 @@ import java.util.Scanner;
 
 public class ChatRoom{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        String s = sc.nextLine();
-        String target = "hello";
-        int idx = 0;
-
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i) == target.charAt(idx))
-                idx++;
+        try (Scanner sc = new Scanner(System.in)) {
+            String s = sc.nextLine();
+            String target = "hello";
+            int idx = 0;
+            
+            for(int i=0;i<s.length();i++){
+                if(s.charAt(i) == target.charAt(idx))
+                    idx++;
+                if(idx == target.length())
+                    break;
+            }
+            
             if(idx == target.length())
-                break;
+                System.out.println("YES");
+            else
+                System.out.println("NO");
         }
-
-        if(idx == target.length())
-            System.out.println("YES");
-        else
-            System.out.println("NO");
-
-        sc.close();
     }
 }
