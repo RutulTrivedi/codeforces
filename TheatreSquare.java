17e99cfaf4d588cfe593e3feab546beb93d95641
RutulTrivedi <rutultrivedi7@gmail.com>
2024-08-28 19:58:44 +0530

    Theatre Square

--- TheatreSquare.java
+++ TheatreSquare.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 
 public class TheatreSquare{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        long n = sc.nextLong();
-        long m = sc.nextLong();
-        long a = sc.nextLong();
+        try (Scanner sc = new Scanner(System.in)) {
+            long n = sc.nextLong();
+            long m = sc.nextLong();
+            long a = sc.nextLong();
 
-        long ans = (long) Math.ceil((double)n/a) * (long) Math.ceil((double)m/a);
-        System.out.println(ans);
+            long ans = (long) Math.ceil((double)n/a) * (long) Math.ceil((double)m/a);
+            System.out.println(ans);
+        }
     }
 }
